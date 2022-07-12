45db7d1694cd47ac58ce54886e9a57fa321b1502
Oladipupo Olamoyegun <104556271+Olajohn2804@users.noreply.github.com>
2022-07-12 11:06:54 +0100

    updated the todolist and the javascript

--- collabo.js
+++ collabo.js
@@ -9,7 +9,10 @@ let allClients=[]
 			firstname:fnam.value,
 			surname:snam.value,
 			email:emai.value,
-			password:pass.value
+			password:pass.value,
+			contact:[],
+			todos:[],
+			
 		}
 		let confirm=confir.value
 		let passwo=pass.value
@@ -42,7 +45,7 @@ function signin(){
 		 if (found==false){
 		 		incorrect.innerText=('Invalid Email or Invalid Password')
 		 	}else{
-		 		location.assign('dashboard.html')
+		 		location.assign('index.html')
 		 		emai.value=''
 		 		pass.value=''
 		 	}
@@ -79,7 +82,7 @@ function signin(){
 			let newContacts={
 				name:nam.value ,
 				phonenumber:phone.value,
-				email:eMail.value
+				email:eMail.value,
 			}
 			if(nam.value==''||phone.value==''){
 				innertext.innerText=('Kindly fill in both name and number')
@@ -123,3 +126,46 @@ function signin(){
 		
 
 }
+// contact javascript ends
+
+
+// to-do javascripts begins
+	var todoArray = [];
+	addTodo = ()=> {
+		if(todoInput.value == ""){
+			alert("enter a value")
+		}else{
+			let userTodo = todoInput.value;
+			todoItem = {item:userTodo, done:false};
+			todoArray.push(todoItem);
+			todoInput.value = "";	
+			showTodos();
+		}
+	}
+ 
+	showTodos = ()=> {
+		 todoInfo = "";
+		 for(i=0;i<todoArray.length;i++){
+			if(todoArray[i].done){
+				todoInfo += `<div class="shadow p-3 mb-1 text-success">${todoArray[i].item}<button class="float-end btn btn-success btn-sm">DONE</button></div>`;
+			}else{
+			todoInfo += `<div class="shadow p-3 mb-1 text-danger">${todoArray[i].item}<button class="float-end btn-danger btn-sm" onclick="markDone(${i})">MARK AS DONE</button></div>`;
+			}
+		 }
+		 displayTodo.innerHTML = todoInfo;
+		 showCount();
+	}
+	showCount = ()=>{
+		let pending = todoArray.filter(todo => !todo.done)
+		taskCount.innerHTML = pending.length;
+	}
+	clearTodo = ()=>{
+		todoArray = [];
+		showTodos();
+	}
+	markDone = (index)=>{
+		todoArray[index].done = true;
+		// console.log(todoArray[index])
+		showTodos();
+	}
+// to-do javascripts ends
